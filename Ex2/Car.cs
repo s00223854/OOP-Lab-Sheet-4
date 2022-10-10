45081a6075f66fba99063c8b3e209c400b5e6af4
eoinkilgallonITS <eoinkilgallon@gmail.com>
2022-10-10 11:14:19 +0100

    Ex2 started

---
+++ Ex2/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_Sheet_4
+{
+    
+    public class Car
+    {
+        private string carMake;
+
+        public string CarMake
+        {
+            get { return this.carMake; }
+            set { this.carMake = value; }
+        }
+
+        private string carModel;
+
+        public string CarModel
+        {
+            get { return this.carModel; }
+            set { this.carModel = value; }
+        }
+
+        private int currentSpeed;
+
+        public int CurrentSpeed
+        {
+            get { return this.currentSpeed; }
+            set { this.currentSpeed = value; }
+        }
+
+        private double engineSize;
+
+        public double EngineSize
+        {
+            get { return this.engineSize; }
+            set { this.engineSize = value; }
+        }
+
+        static void DisplayInfo()
+        { }
+    }
+}
