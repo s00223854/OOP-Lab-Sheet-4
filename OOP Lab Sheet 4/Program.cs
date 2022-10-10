ef3f139ee55e93d6d39e4d4fcea75f4213688631
eoinkilgallonITS <eoinkilgallon@gmail.com>
2022-10-10 10:54:59 +0100

    Ex 1 Working

--- OOP Lab Sheet 4/Program.cs
+++ OOP Lab Sheet 4/Program.cs
@@ -14,7 +14,39 @@ namespace OOP_Lab_Sheet_4
     }
     class Car
     {
+        private string carMake;
 
+        public string CarMake
+        {
+            get { return this.carMake; }
+            set { this.carMake = value; }
+        }
 
+        private string carModel;
+
+        public string CarModel
+        {
+            get { return this.carModel; }
+            set { this.carModel = value; }
+        }
+
+        private int currentSpeed;
+
+        public int CurrentSpeed
+        {
+            get { return this.currentSpeed; }
+            set { this.currentSpeed = value; }
+        }
+
+        private double engineSize;
+
+        public double EngineSize
+        {
+            get { return this.engineSize; }
+            set { this.engineSize = value; }
+        }
+
+        static void DisplayInfo()
+        { }
     }
 }
