eed1657039fd57cf71aee4f021d362e1cab7792d
eoinkilgallonITS <eoinkilgallon@gmail.com>
2022-10-10 11:50:55 +0100

    Ex3 started

---
+++ Ex3/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    internal class  BankAccount
+    {
+        private int accountNumber;
+
+        public int AccountNumber
+        {
+            get { return this.accountNumber; }
+            set { this.accountNumber = value; }
+        }
+
+        private string accountHolder;
+
+        public string AccountHolder
+        {
+            get { return this.accountHolder; }
+            set { this.accountHolder = value; }
+        }
+
+        private double balance;
+
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+    }
+}
