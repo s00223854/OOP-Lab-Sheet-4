7cfbfe682d56d186dbfc95970c6573926112ac8b
eoinkilgallonITS <eoinkilgallon@gmail.com>
2022-10-10 11:59:42 +0100

    Ex3 Working

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -10,6 +10,16 @@ namespace Ex3
     {
         static void Main(string[] args)
         {
+            BankAccount a1 = new BankAccount();
+            a1.AccountNumber = 123;
+            a1.AccountHolder = "Bob";
+            a1.Balance = 20.1;
+
+            BankAccount a2 = new BankAccount();
+            a2.AccountNumber = 456;
+            a2.AccountHolder = "Beth";
+            a2.Balance = 932.0;
+
         }
     }
 }
